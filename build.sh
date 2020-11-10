e07a80071cd9b147818e4c6a480c0972c8043550
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2020-11-10 18:52:56 -0300

    Bug fixes.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./grgradlew -PrepositoryUrl=$REPOSITORY_URL -PrepositoryUser=$REPOSITORY_USER -PrepositoryPassword=$REPOSITORY_PASSWORD build publishToMavenLocal uploadArchives
+./gradlew -PrepositoryUrl=$REPOSITORY_URL -PrepositoryUser=$REPOSITORY_USER -PrepositoryPassword=$REPOSITORY_PASSWORD build publishToMavenLocal uploadArchives
