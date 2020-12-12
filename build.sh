8e38f55bb6d199bbeda370f96b5978c04cb70876
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2020-12-12 16:22:47 -0300

    Updates.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-./gradlew -PrepositoryUrl=$REPOSITORY_URL -PrepositoryUser=$REPOSITORY_USER -PrepositoryPassword=$REPOSITORY_PASSWORD build publishToMavenLocal uploadArchives
+if [ ! -z "$REPOSITORY_URL" ]; then 
+	./gradlew -PrepositoryUrl=$REPOSITORY_URL -PrepositoryUser=$REPOSITORY_USER -PrepositoryPassword=$REPOSITORY_PASSWORD clean build publishToMavenLocal
+else
+	./gradlew clean build publishToMavenLocal
+fi	
