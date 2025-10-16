d06374b666fa3385a90b690a73111c2a2382c79a
Felipe Vilarinho <fvilarinho@gmail.com>
2025-10-16 20:55:38 -0300

    Code improvements.

--- src/main/java/br/com/concepting/framework/exceptions/InternalErrorException.java
+++ src/main/java/br/com/concepting/framework/exceptions/InternalErrorException.java
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -22,7 +22,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see http://www.gnu.org/licenses.</pre>
+ * along with this program.  If not, see <a href="http://www.gnu.org/licenses"></a>.</pre>
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "class")
 @JsonIgnoreProperties({"stackTrace", "suppressed", "cause"})
