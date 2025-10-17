a112d8f1534b27004761510ad7ab8781e934d555
Felipe Vilarinho <fvilarinho@gmail.com>
2025-10-16 21:18:15 -0300

    Code improvements.

--- src/main/java/br/com/concepting/framework/security/exceptions/UserNotAuthorizedException.java
+++ src/main/java/br/com/concepting/framework/security/exceptions/UserNotAuthorizedException.java
@@ -13,7 +13,7 @@ import br.com.concepting.framework.exceptions.ExpectedErrorException;
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -22,7 +22,7 @@ import br.com.concepting.framework.exceptions.ExpectedErrorException;
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see http://www.gnu.org/licenses.</pre>
+ * along with this program.  If not, see <a href="http://www.gnu.org/licenses"></a>.</pre>
  */
 public class UserNotAuthorizedException extends ExpectedErrorException{
     private static final long serialVersionUID = 2218995807861634081L;
