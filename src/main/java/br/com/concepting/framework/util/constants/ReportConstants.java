23d4af9ff6e69bb5c213b05a964f9d931f99f765
Felipe Vilarinho <fvilarinho@gmail.com>
2025-10-16 22:06:43 -0300

    Code improvements.

--- src/main/java/br/com/concepting/framework/util/constants/ReportConstants.java
+++ src/main/java/br/com/concepting/framework/util/constants/ReportConstants.java
@@ -14,7 +14,7 @@ import br.com.concepting.framework.util.types.ContentType;
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -23,7 +23,7 @@ import br.com.concepting.framework.util.types.ContentType;
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see http://www.gnu.org/licenses.</pre>
+ * along with this program.  If not, see <a href="http://www.gnu.org/licenses"></a>.</pre>
  */
 public abstract class ReportConstants{
     public static final String EXPORT_TYPE_ATTRIBUTE_ID = "exportType";
