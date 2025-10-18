afca030d94b1901aeff75fdd25981b54efa5583b
Felipe Vilarinho <fvilarinho@gmail.com>
2025-10-18 09:58:08 -0300

    Code improvements.

--- src/main/java/br/com/concepting/framework/util/helpers/DateTime.java
+++ src/main/java/br/com/concepting/framework/util/helpers/DateTime.java
@@ -14,7 +14,7 @@ import java.util.Date;
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -23,7 +23,7 @@ import java.util.Date;
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see http://www.gnu.org/licenses.</pre>
+ * along with this program.  If not, see <a href="http://www.gnu.org/licenses"></a>.</pre>
  */
 @JsonDeserialize(using = DateTimeDeserializer.class)
 public class DateTime extends Date{
