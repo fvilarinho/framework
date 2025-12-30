fc36e4668c79568dec958e77a6e72ef764dd620d
Felipe Vilarinho <fvilarinho@gmail.com>
2025-12-30 12:18:22 -0300

    Update pipeline.

--- codeAnalysis.sh
+++ codeAnalysis.sh
@@ -54,9 +54,7 @@ function codeAnalysis() {
   SONAR_PROJECT_KEY="$SONAR_ORGANIZATION$SEPARATOR$buildName"
   QUALITY_GATE_STATUS=$($CURL_CMD -s \
                                   -H "Authorization: Bearer $SONAR_TOKEN" \
-                                  "$SONAR_URL/api/qualitygates/project_status?projectKey=$SONAR_PROJECT_KEY")
-
-  echo $QUALITY_GATE_STATUS
+                                  "$SONAR_URL/api/qualitygates/project_status?projectKey=$SONAR_PROJECT_KEY" | $JQ_CMD -r '.projectStatus.status')
 
   if [ -z $QUALITY_GATE_STATUS ] || [ "$QUALITY_GATE_STATUS" == "ERROR" ]; then
     echo
